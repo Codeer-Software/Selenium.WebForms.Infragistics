0945efb151052413f81d294672f9a38eff1af76c
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2016-05-09 23:35:37 +0900

    Version up.

--- Project/Selenium.WebForms.Infragistics/Properties/AssemblyInfo.cs
+++ Project/Selenium.WebForms.Infragistics/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("e4fe2124-d2f7-48df-a96a-3a161f65fb53")]
 
-[assembly: AssemblyVersion("0.0.37.0")]
-[assembly: AssemblyFileVersion("0.0.37.0")]
+[assembly: AssemblyVersion("0.0.38.0")]
+[assembly: AssemblyFileVersion("0.0.38.0")]
